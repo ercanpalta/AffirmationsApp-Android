7a7840492acee206a52bb926f26bd77b2bfca910
Ercan Palta <ercan.palta@outlook.com>
2022-06-23 19:18:10 +0300

    Unit and instrumentation tests implemented

---
+++ app/src/test/java/com/ercanpalta/affirmations/AffirmationsAdapterTests.kt
@@ -0,0 +1,22 @@
+package com.ercanpalta.affirmations
+
+import android.content.Context
+import com.ercanpalta.affirmations.adapter.ItemAdapter
+import com.ercanpalta.affirmations.model.Affirmation
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+class AffirmationsAdapterTests {
+    private val context = mock(Context::class.java)
+
+    @Test
+    fun adapter_size(){
+        val data = listOf(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2)
+        )
+        val adapter = ItemAdapter(context,data)
+        assertEquals("ItemAdapter is not the correct size", data.size, adapter.itemCount)
+    }
+}
